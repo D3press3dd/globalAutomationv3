d6b1d1b4ada1198402fa855a9a07caacfbadba1d
D3press3dd <anthonyrosman20@gmail.com>
2021-10-30 12:04:15 -0400

    sin animacion en js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,29 +35,6 @@
   };
 
   /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select("#navbar .scrollto", true);
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return;
-      let section = select(navbarlink.hash);
-      if (!section) return;
-      if (
-        position >= section.offsetTop &&
-        position <= section.offsetTop + section.offsetHeight
-      ) {
-        navbarlink.classList.add("active");
-      } else {
-        navbarlink.classList.remove("active");
-      }
-    });
-  };
-  window.addEventListener("load", navbarlinksActive);
-  onscroll(document, navbarlinksActive);
-
-  /**
    * Scrolls to an element with header offset
    */
   const scrollto = el => {
@@ -73,18 +50,18 @@
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-  let selectHeader = select("#header");
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 10) {
-        selectHeader.classList.add("header-scrolled");
-      } else {
-        selectHeader.classList.remove("header-scrolled");
-      }
-    };
-    window.addEventListener("load", headerScrolled);
-    onscroll(document, headerScrolled);
-  }
+  //!desde aqui let selectHeader = select("#header");
+  // if (selectHeader) {
+  //   const headerScrolled = () => {
+  //     if (window.scrollY > 10) {
+  //       selectHeader.classList.add("header-scrolled");
+  //     } else {
+  //       selectHeader.classList.remove("header-scrolled");
+  //     }
+  //   };
+  //   window.addEventListener("load", headerScrolled);
+  //   onscroll(document, headerScrolled);
+  // }
 
   /**
    * Back to top button
@@ -161,77 +138,8 @@
   });
 
   /**
-   * Gallery Slider
-   */
-  new Swiper(".gallery-slider", {
-    speed: 400,
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-      },
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      992: {
-        slidesPerView: 5,
-        spaceBetween: 30,
-      },
-      1200: {
-        slidesPerView: 7,
-        spaceBetween: 30,
-      },
-    },
-  });
-
-  /**
-   * Initiate gallery lightbox
-   */
-  const galleryLightbox = GLightbox({
-    selector: ".gallery-lightbox",
-  });
-
-  /**
    * Testimonials slider
    */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40,
-      },
-
-      1200: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-    },
-  });
 
   /**
    * Animation on scroll
